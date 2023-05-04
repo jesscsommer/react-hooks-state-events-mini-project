ded6740f286b674699973ed3f94c3313b44fbf1c
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-04 08:24:05 -0700

    Display and remove tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-function Task() {
+function Task({ category, text, removeTask }) {
+
   return (
     <div className="task">
-      <div className="label">CATEGORY HERE</div>
-      <div className="text">TEXT HERE</div>
-      <button className="delete">X</button>
+      <div className="label">{category}</div>
+      <div className="text">{text}</div>
+      <button name={text} onClick={(e) => removeTask(e.target.name)} className="delete">X</button>
     </div>
   );
 }
